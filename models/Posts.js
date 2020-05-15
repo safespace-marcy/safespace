3e07486ca843b1833546cb5baa1403bd1349ed2b
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-05-15 02:49:47 -0400

    refactor post queries

---
+++ models/Posts.js
@@ -0,0 +1,36 @@
+const db = require('../db')
+
+class Post {
+  static create (userId, communityId, content) {
+    const queryText = 'INSERT INTO posts (user_id, community_id, content) VALUES ($1, $2, $3);'
+    return db.query(queryText, [userId, communityId, content])
+  }
+
+  static delete (postId) {
+    const queryText = 'DELETE FROM posts WHERE id = $1;'
+    return db.query(queryText, [postId])
+  }
+
+  static update (content, postId) {
+    const queryText = 'UPDATE posts SET content = $1 WHERE id = $2'
+    return db.query(queryText, [content, postId])
+  }
+
+  static getById (postId) {
+    const queryText = 'SELECT * FROM posts WHERE id = $1;'
+    return db.query(queryText, [postId])
+      .then((response) => response.rows[0])
+  }
+
+  static getAllByUser (userId) {
+    const queryText = 'SELECT * FROM posts WHERE user_id = $1;'
+    return db.query(queryText, [userId])
+      .then((response) => response.rows)
+  }
+
+  static getAllByCommunity (communityId) {
+    const queryText = 'SELECT * FROM posts WHERE community_id = $1'
+    return db.query(queryText, [communityId]).then(response => response.rows)
+  }
+}
+module.exports = Post
