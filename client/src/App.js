1c89188928b620675df4d04f9c19d76312c8de18
Devonte Duncan <devonteduncan202@gmail.com>
2020-05-16 03:34:30 -0400

    (feat) creates react router architechture

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import Login from './components/Login'
 import Landing from './components/Landing'
+import Navbar from './components/Navbar'
 
 function App () {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
+        <Navbar />
 
         <Switch>
           <Route path="/login">
