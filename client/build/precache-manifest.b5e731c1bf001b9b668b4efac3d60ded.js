2abed3c8c6cdc99112a18ca7672208348a60e78b
Devonte Duncan <devonteduncan202@gmail.com>
2020-05-17 18:37:45 -0400

    (install) adds yoga framework to package.json

---
+++ client/build/precache-manifest.b5e731c1bf001b9b668b4efac3d60ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e72ab83e41b2baf7c7884da38a54f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c3d01deead38cacef0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "70c3d01deead38cacef0",
+    "url": "/static/js/2.93f25013.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.93f25013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9822efd36abd3dfafe5",
+    "url": "/static/js/main.4cf4585a.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
