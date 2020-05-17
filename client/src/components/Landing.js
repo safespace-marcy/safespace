7284839b08acc9ee44ea4fdbb7d66e256321561f
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-05-16 20:48:34 -0400

    pull user context

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../contexts/userContext'
 
 const Landing = () => {
+  const { user } = useContext(UserContext)
+
   return (
     <div>
       <h1>This is the landing page placeholder!</h1>
+      {user && <p>Hello {user.username}</p>}
     </div>
   )
 }
