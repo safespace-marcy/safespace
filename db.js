8186fd47c7f4422fea58859b8bc90119bef1e35e
EC2 Default User <cieloraymundo12@gmail.com>
2020-05-17 14:07:56 +0000

    updated db.js file

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
 const { Pool } = require('pg')
 
-// const pool = new Pool({
-//   connectionString: process.env.DB_URL
-// })
-
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'safe_space',
-  password: null,
-  port: 5432
-});
+  connectionString: process.env.DB_URL
+})
 
 module.exports = {
   query: (text, params) => {
