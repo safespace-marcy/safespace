a1ab8174025cc8569a150376f0a63dadbe9355bc
EC2 Default User <cieloraymundo12@gmail.com>
2020-05-24 14:26:31 +0000

    (feat-#66) added the read more feature

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import ReadMoreReact from 'read-more-react';
 import { Item } from 'semantic-ui-react'
-import { Card } from 'react-bootstrap'
+import { Card, Nav } from 'react-bootstrap'
 
 function Post (props) {
   const dateCreated = new Date(props.data.created_at)
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
   const [userResponse, setUserResponse] = useState(null)
    
   const getUser = async () => {
@@ -17,10 +18,15 @@ function Post (props) {
   
   useEffect(() => {
     getUser()
-  }, [])
+  },[])
   
   return (
       <Item style={{width:"80%", marginLeft:"10%", marginTop:"30px", border:"2px solid black", padding:"15px", boxShadow:"2px 5px #888888"}}>
+        {userResponse ? (
+          <LinkContainer style={{ color: 'white' }} to={`/user/${userResponse.id}`}>
+            <Nav.Link><h6 class="card-subtitle mb-2 text-muted">{userResponse.username}</h6></Nav.Link>
+          </LinkContainer>
+        ) : ''}
         <Item.Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
         <Item.Content>
           <Item.Header as='a'>{props.data.title}</Item.Header>
@@ -28,8 +34,14 @@ function Post (props) {
             {months[dateCreated.getMonth()]} {dateCreated.getDate()}, {dateCreated.getFullYear()} {dateCreated.getHours()}:{dateCreated.getMinutes()} {dateCreated.getHours() >= 12 ? "PM" : "AM"}
           </Item.Meta>
           <Item.Description>
-            <p>{props.data.content}</p>
-          </Item.Description>
+            <ReadMoreReact 
+              text={props.data.content}
+              min={200}
+              max={500}
+              ideal={300}
+              readMoreText="...See More"
+            />
+          </Item.Description> 
           <Item.Extra>
             <Card.Link href="#">Like</Card.Link>
             <Card.Link href="#">Comments</Card.Link>
