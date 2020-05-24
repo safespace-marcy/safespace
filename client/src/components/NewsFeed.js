188745eab176254f867c739297339d31f6796300
EC2 Default User <cieloraymundo12@gmail.com>
2020-05-24 14:23:30 +0000

    formated pages

--- client/src/components/NewsFeed.js
+++ client/src/components/NewsFeed.js
@@ -3,6 +3,7 @@ import Post from './Post'
 import NewPostModal from './NewPostModal'
 import { useParams } from 'react-router-dom'
 import { Loader } from 'semantic-ui-react'
+import { Item } from 'semantic-ui-react'
 
 function NewsFeed () {
   const [response, setResponse] = useState(null)
@@ -19,9 +20,6 @@ function NewsFeed () {
     }
     getPost()
   }, [id])
-
-
-  console.log(response);
   
   return response === null ? (
     <div>
@@ -30,9 +28,11 @@ function NewsFeed () {
   ) : (
     <>
       <NewPostModal setNewPost={setNewPost} />
-      {response.map((res, i) => (
-        <Post key={res.title + i} data={res}/>
-      ))}
+      <Item.Group>
+        {response.map((res, i) => (
+          <Post key={res.title + i} data={res}/>
+        ))}
+      </Item.Group>
     </>
   )
 }
