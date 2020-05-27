d61a9dd0ddda80d78e3230933a74cb033c2a4fb2
Devonte Duncan <devonteduncan202@gmail.com>
2020-05-27 01:27:26 -0400

    (feat) adds private chat feature

--- socketManager.js
+++ socketManager.js
@@ -1,10 +1,36 @@
 const io = require('./server.js').io
 
+const onlineUsers = []
+
 module.exports = (socket) => {
   // Relays the socket id to the connected client
   socket.emit('your id', socket.id)
-  // Sends the message to all connected users
-  socket.on('send message', (body) => {
-    io.emit('message', body)
+  // Saves connected user to an array
+  socket.on('online', (userRef) => {
+    for(let i = 0; i < onlineUsers.length; i+=1){
+      if(onlineUsers[i].userId === userRef.userId){
+        socket.broadcast.emit('update', onlineUsers)
+        return 'Already Online'
+      }
+    }
+    onlineUsers.push(userRef)
+    socket.broadcast.emit('update', onlineUsers)
+    console.log(onlineUsers)
   })
+  // Sends the message to sender and specified reciever
+  socket.on('send message', (body) => {
+    io.to(socket.id).emit('message', body)
+    io.to(body.socketId).emit('message', body)
+  })
+  // Removes users from onlineUsers array upon disconnection
+  socket.on('disconnect', () => {
+    for(let i = 0; i < onlineUsers.length; i+=1){
+      if(onlineUsers[i].socketId === socket.id){
+        onlineUsers.splice(i, 1)
+      }
+    }
+    socket.emit('update', onlineUsers)
+    console.log(onlineUsers)
+  })
+
 }
